5aa60b91757f6c08d45d97ef871a8334cafc97ee
Samuel Francis <samuelmaled32@gmail.com>
2026-08-19 19:25:57 +0200

    test

---
+++ config.js
@@ -0,0 +1,16 @@
+window.MYSTERY_CONFIG = {
+  totalBoxes: 8000,
+  teams: {
+    "1": {
+      name: "فريق 1",
+      words: {
+        "3": "سااام عمك",
+        "10": "نور",
+        "11": "1 🌟",
+        "12": { type: "emoji", value: "🔑" },
+        "13": { type: "image", src: "https://via.placeholder.com/64.png?text=IMG", alt: "رمز صورة" },
+        "7999": "مبروك"
+      }
+    }
+  }
+};
